db1d1c6f8bee9b0f7f667fcc5c7b6689cc70dc9a
RufusSteels <52851612+RufusSteels@users.noreply.github.com>
2025-05-15 18:26:10 +0200

    [MOD] fix short pages [ADD] page transitions

---
+++ transition.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+
+    const transition_element_load = document.querySelector(".transition-load");
+    const transition_element_leave = document.querySelector(".transition-leave");
+    const anchors = document.querySelectorAll("a");
+
+    if (document.readyState === "loading")
+    {
+        document.addEventListener("DOMContentLoaded", (event) => {
+            transition_element_load.classList.remove("is-active");
+        });
+    }
+    else {
+        transition_element_load.classList.remove("is-active");
+    }
+
+    for (let i = 0; i < anchors.length; i++) {
+        anchors[i].addEventListener("click", e => {
+            e.preventDefault();
+
+            transition_element_leave.classList.add("is-active");
+            
+            setTimeout(() => {
+                window.location.href = e.target.href;
+            }, 300);
+        });
+    }
+}
+
